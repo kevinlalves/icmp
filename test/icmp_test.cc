c0836f9dedc408849c6340e0d8bb638aa7710faf
Kevin de Lima Alves <kevin.lima@lds.ifce.edu.br>
2021-12-01 23:55:04 -0300

    little fix

--- test/icmp_test.cc
+++ test/icmp_test.cc
@@ -4,8 +4,7 @@
 
 TEST(IcmpTest, Encode) {
     Icmp packet(0x8, 0x0, 0xa2b3, 0x0);
-    uint8_t temp[] = {8, 0, 0x55, 0x4c, 0xa2, 0xb3, 0, 0};
-    std::vector<uint8_t> byte_array(temp, temp+sizeof(temp)); 
+    std::vector<uint8_t> byte_array{8, 0, 0x55, 0x4c, 0xa2, 0xb3, 0, 0}; 
     EXPECT_EQ(packet.Encode(), byte_array);
 
     packet.IncrementSeq();
